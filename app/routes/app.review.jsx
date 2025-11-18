42ef6fe510d566133bd49c968acf2aaf876f4af0
balwansinghrajputITG <balwanrajput@itgeeks.com>
2025-11-18 12:53:48 +0530

    add tapbar

--- app/routes/app.review.jsx
+++ app/routes/app.review.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import { AppProvider, Card, Tabs } from "@shopify/polaris";
-import { useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
+import {
+  AppProvider,
+  Card,
+  Tabs,
+  Page,
+  Box,
+  Text,
+  LegacyCard,
+  LegacyTabs,
+} from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
+import {
+  EmailIcon,
+  SearchIcon,
+  StarFilledIcon,
+  StarIcon,
+} from "@shopify/polaris-icons";
 
-function AppReview() {
+export default function AppReview() {
   const [selected, setSelected] = useState(0);
 
   const handleTabChange = useCallback(
@@ -14,34 +28,45 @@ function AppReview() {
   const tabs = [
     {
       id: "all-customers-1",
-      content: "All",
+      content: (
+        <Box style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+          <StarFilledIcon width="18" /> Rewiew
+        </Box>
+      ),
       accessibilityLabel: "All customers",
       panelID: "all-customers-content-1",
     },
     {
       id: "accepts-marketing-1",
-      content: "Accepts marketing",
+      content: (
+        <Box style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+          <EmailIcon width="18" /> Reviewe Requested
+        </Box>
+      ),
       panelID: "accepts-marketing-content-1",
     },
     {
       id: "repeat-customers-1",
-      content: "Repeat customers",
+      content: (
+        <Box style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+          <SearchIcon width="18" /> Costomers Question
+        </Box>
+      ),
       panelID: "repeat-customers-content-1",
-    },
-    {
-      id: "prospects-1",
-      content: "Prospects",
-      panelID: "prospects-content-1",
     },
   ];
 
   return (
     <AppProvider>
-      <Card>
-        <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}></Tabs>
-      </Card>
+      <Page fullWidth={true}>
+        <Card padding="50">
+          <LegacyTabs
+            tabs={tabs}
+            selected={selected}
+            onSelect={handleTabChange}
+          ></LegacyTabs>
+        </Card>
+      </Page>
     </AppProvider>
   );
 }
-
-export default AppReview;
