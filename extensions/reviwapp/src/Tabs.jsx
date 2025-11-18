fbd2ce19a44cf61d2be03b51478fe0554d3e5db9
ManishKumarITG <manishkumar@itgeeks.com>
2025-11-18 11:14:22 +0530

    save data

---
+++ extensions/reviwapp/src/Tabs.jsx
@@ -0,0 +1,16 @@
+import "@shopify/ui-extensions/preact";
+import { render } from "preact";
+
+export default async () => {
+  render(<TabShifter />, document.body);
+};
+
+function TabShifter() {
+  return (
+    <s-banner>
+      <s-text>
+        uyt7u68i6i68768ossd fasdfasdfsdfasdfasdf
+      </s-text>
+    </s-banner>
+  );
+}
