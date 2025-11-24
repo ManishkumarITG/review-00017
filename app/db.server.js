3e2a36c679266d29a6a6846c48da96c6f26be53e
RohanvermaITG <rohanverma@itgeeks.com>
2025-11-24 13:48:40 +0530

    make write a review extension

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+dotenv.config();
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+import mongoose from "mongoose"
+
+// Function to establish a connection to the MongoDB database
+export const dbConnection = async () => {
+  try {
+    // Using mongoose.connect to establish a connection to the MongoDB database
+    //const connectionString = process.env.NODE_ENV !== 'production' ? `${process.env.DB_URL}/${process.env.DB_NAME}?authSource=admin` : process.env.DB_URL;
+    const connectionString = `${process.env.DB_URL}/${process.env.DB_NAME}?authSource=admin`;
+
+
+
+    console.log(connectionString, '::: --- DataBase connection string ---');
+
+    // Connect to the MongoDB database
+    await mongoose.connect(connectionString);
+
+    // Log a success message once the connection is successful
+    console.log(`--- Connected to MongoDB (${process.env.NODE_ENV}) Successfully ---`);
+  } catch (error) {
+    console.error(error, `--- MongoDB Connection Failed (${process.env.NODE_ENV}) ---`);
   }
-}
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
-
-export default prisma;
+};
+dbConnection();
+export default dbConnection
--- app/shopify.server.js
+++ app/shopify.server.js
@@ -4,7 +4,9 @@ import {
   AppDistribution,
   shopifyApp,
 } from "@shopify/shopify-app-react-router/server";
-import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+// import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
+
 import prisma from "./db.server";
 
 const shopify = shopifyApp({
@@ -14,7 +16,10 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new MongoDBSessionStorage(
+    process.env.DB_URL,
+    process.env.DB_NAME,
+  ),
   distribution: AppDistribution.AppStore,
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
