7a59f143e16c66b85f7677f99792b8e8e85b0d53
Mohit Singh <mohitsingh@itgeeks.com>
2025-11-26 16:34:25 +0530

    api confug

---
+++ app/api/test.$.js
@@ -0,0 +1,19 @@
+
+
+export const loader = async ({ request }) => {
+    try {
+        console.log('-----------------------------------------------------------hit api');
+        
+        return new Response(JSON.stringify({ status: true, message: "ok" }), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST,PUT,DELETE, OPTIONS",
+            },
+        });
+    } catch (error) {
+        console.log("catch error in test loader :", error);
+
+    }
+};
