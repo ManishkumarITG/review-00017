1c04b0d65e3013ba9a712d8491cadab04769ad6b
santoshmodi00 <158157439+santoshmodi00@users.noreply.github.com>
2026-05-05 15:56:26 +0530

    fix: update user.controller.js to use correct service function names

--- app/api/controller/user.controller.js
+++ app/api/controller/user.controller.js
@@ -1,78 +1,63 @@
 import {
-  createUser,
-  getAllUsers,
-  getUserById,
-  updateUserById,
-  deleteUserById,
+    createOrGetUser,
+    getUserByCustomerId,
+    updateUser,
+    deleteUser,
 } from "../services/user.service.js";
 
 import STATUS_CODE from "../contents/statusCode.js";
 import MESSAGE from "../contents/message.js";
 import { responseHandler } from "../utils/responseHandler.js";
 
-// Create User
+// Create or Get User
 export const createUserController = async (payload) => {
-  try {
-    const data = await createUser(payload);
-    return responseHandler(STATUS_CODE.CREATED, MESSAGE.CREATED, data);
-  } catch (error) {
-    console.log("Error in create user controller", error);
-    return responseHandler(
-      STATUS_CODE.INTERNAL_SERVER_ERROR,
-      error.message,
-      null,
-    );
-  }
+    try {
+          const data = await createOrGetUser(payload);
+          return responseHandler(STATUS_CODE.CREATED, MESSAGE.CREATED, data);
+    } catch (error) {
+          console.log("Error in create user controller", error);
+          return responseHandler(
+                  STATUS_CODE.INTERNAL_SERVER_ERROR,
+                  error.message,
+                  null,
+                );
+    }
 };
 
-// Get All Users
-export const getUsersController = async () => {
-  try {
-    const data = await getAllUsers();
-    return responseHandler(STATUS_CODE.OK, MESSAGE.SUCCESS, data);
-  } catch (error) {
-    return responseHandler(
-      STATUS_CODE.INTERNAL_SERVER_ERROR,
-      MESSAGE.SERVER_ERROR,
-      null,
-    );
-  }
-};
-
-// Get Single User
-export const getUserController = async (id) => {
-  try {
-    const data = await getUserById(id);
-    return responseHandler(STATUS_CODE.OK, MESSAGE.SUCCESS, data);
-  } catch (error) {
-    return responseHandler(
-      STATUS_CODE.INTERNAL_SERVER_ERROR,
-      error.message,
-      null,
-    );
-  }
+// Get Single User by Customer ID
+export const getUserController = async (customerId) => {
+    try {
+          const data = await getUserByCustomerId(customerId);
+          return responseHandler(STATUS_CODE.OK, MESSAGE.SUCCESS, data);
+    } catch (error) {
+          return responseHandler(
+                  STATUS_CODE.INTERNAL_SERVER_ERROR,
+                  error.message,
+                  null,
+                );
+    }
 };
 
 // Update User
-export const updateUserController = async (id, payload) => {
-  try {
-    const data = await updateUserById(id, payload);
-    return responseHandler(STATUS_CODE.OK, MESSAGE.UPDATED, data);
-  } catch (error) {
-    return responseHandler(STATUS_CODE.BAD_REQUEST, error.message, null);
-  }
+export const updateUserController = async (customerId, payload) => {
+    try {
+          const data = await updateUser(customerId, payload);
+          return responseHandler(STATUS_CODE.OK, MESSAGE.UPDATED, data);
+    } catch (error) {
+          return responseHandler(STATUS_CODE.BAD_REQUEST, error.message, null);
+    }
 };
 
 // Delete User
-export const deleteUserController = async (id) => {
-  try {
-    const data = await deleteUserById(id);
-    return responseHandler(STATUS_CODE.OK, MESSAGE.DELETED, data);
-  } catch (error) {
-    return responseHandler(
-      STATUS_CODE.INTERNAL_SERVER_ERROR,
-      error.message,
-      null,
-    );
-  }
+export const deleteUserController = async (customerId) => {
+    try {
+          const data = await deleteUser(customerId);
+          return responseHandler(STATUS_CODE.OK, MESSAGE.DELETED, data);
+    } catch (error) {
+          return responseHandler(
+                  STATUS_CODE.INTERNAL_SERVER_ERROR,
+                  error.message,
+                  null,
+                );
+    }
 };
